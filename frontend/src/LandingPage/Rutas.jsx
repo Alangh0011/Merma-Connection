97cd84ac869b37dd69165baea65bf891121bb4b2
fersito <fernandoreyesvivar@gmail.com>
2024-04-14 10:36:22 -0600

    se agrega olanding page

---
+++ frontend/src/LandingPage/Rutas.jsx
@@ -0,0 +1,23 @@
+import About from './About';
+import AllInOne from './AllInOne';
+import Footer from './Footer';
+import Hero from './Hero';
+import Navbar from './Navbar'
+import Pricing from './Pricing';
+import Support from './Support'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

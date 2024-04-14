c700d169a0b477d74e22f1cbe451639166e3c6f4
Ricardo Mora <rurimoca@gmail.com>
2024-04-14 12:13:59 -0600

    Navbar más limpio

--- frontend/src/User/Navbar.jsx
+++ frontend/src/User/Navbar.jsx
@@ -15,13 +15,6 @@ const Navbar = () => {
       <div className='px-2 flex justify-between items-center w-full h-full'>
         <div className='flex items-center'>
           <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Merma-Connect.</h1>
-          <ul className='hidden md:flex'>
-          <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-          <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
-          <li><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
-          <li><Link to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link></li>
-          <li><Link to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
-          </ul>
         </div>
         <div className='hidden md:flex pr-4'>
           <button className='border-none bg-transparent text-black mr-4'>
